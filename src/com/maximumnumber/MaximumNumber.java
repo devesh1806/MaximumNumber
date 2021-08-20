f8e06d557236fc35141237056f9c7fc82386aa91
Devesh Bhangale1 <devesh.bhangale1@morningstar.com>
2021-08-20 21:42:06 +0530

    [Devesh]: Added maximum of 3 strings checking condition

--- src/com/maximumnumber/MaximumNumber.java
+++ src/com/maximumnumber/MaximumNumber.java
@@ -37,14 +37,35 @@ public class MaximumNumber {
 		System.out.println(max);
 	}	
 	
+	public static void stringmax(String x, String y, String z) {
+		String max = "";
+		if ( x.compareTo(y) == 1 ) {
+			max = x;
+			if ( z.compareTo(max) == 1 ) {
+				max = z;
+			}
+		}
+		else {
+			max = y;
+			if ( z.compareTo(max) == 1 ) {
+				max = z;
+			}
+		}
+		System.out.println(max);
+	}
+	
 	public static void main(String[] args) {
 		
 		//UC1 - checking maximum of 3 numbers
 		Integer x = 2 ; Integer y = 4 ; Integer z = 7;
-		intmax(x,y,z);
+		intmax( x , y , z );
 		
 		//UC2 - checking maximum of 3 floats
 		Float x1 = 8.2f ; Float y1 = 7.5f ; Float z1 = 6.5f;
-		floatmax(x1,y1,z1);
+		floatmax( x1 , y1 , z1 );
+		
+		//UC3 - checking maximum of 3 strings
+		String x2 = "Apple" ; String y2 = "Peach" ; String z2 = "Banana" ;
+		stringmax( x2 , y2 , z2 );
 	}
 }

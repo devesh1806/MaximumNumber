a314beb7d275ac49841cae387043ac0e38801e20
Devesh Bhangale1 <devesh.bhangale1@morningstar.com>
2021-08-20 22:18:06 +0530

    [Devesh]: Refractored class to generic and parameterized constructor to call static maximum

--- src/com/maximumnumber/MaximumNumber.java
+++ src/com/maximumnumber/MaximumNumber.java
@@ -1,8 +1,18 @@
 package com.maximumnumber;
 
-public class MaximumNumber {
+public class MaximumNumber <E extends Comparable<E>> {
 	
+	E x , y , z ;
 	
+	public MaximumNumber(E x, E y, E z) {
+		this.x = x ;
+		this.y = y ;
+		this.z = z ;
+	}
+	
+	public void testMaximum() {
+		testMaximum(x, y, z);
+	}
 	public static <E extends Comparable<E>> void testMaximum(E x, E y, E z) {
 		E max = x;
 		if ( x.compareTo(y) == 1 ) {
@@ -24,6 +34,7 @@ public class MaximumNumber {
 	
 	public static void main(String[] args) {
 		
+		//UC 1-3 and Refractor 1 -- created just generic method 
 		Integer x = 2 ; Integer y = 4 ; Integer z = 7;
 		testMaximum( x , y , z );
 		
@@ -32,5 +43,10 @@ public class MaximumNumber {
 		
 		String x2 = "Apple" ; String y2 = "Peach" ; String z2 = "Banana" ;
 		testMaximum( x2 , y2 , z2 );
+		
+		//Refractor 2 - created class generic and parameterized constructor
+		new MaximumNumber(x, y, z).testMaximum();;
+		new MaximumNumber(x1, y1, z1).testMaximum();;
+		new MaximumNumber(x2, y2, z2).testMaximum();;
 	}
 }

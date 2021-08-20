50fd0e3998f1def9add8743918288a75a9cfe1b0
Devesh Bhangale1 <devesh.bhangale1@morningstar.com>
2021-08-20 21:32:49 +0530

    [Devesh]: Added maximum of 3 floats checking condition

--- src/com/maximumnumber/MaximumNumber.java
+++ src/com/maximumnumber/MaximumNumber.java
@@ -20,10 +20,31 @@ public class MaximumNumber {
 		System.out.println(max);
 	}
 	
+	public static void floatmax(Float x, Float y, Float z) {
+		Float max = Float.MIN_VALUE;
+		if ( x.compareTo(y) == 1 ) {
+			max = x;
+			if ( z.compareTo(max) == 1 ) {
+				max = z;
+			}
+		}
+		else {
+			max = y;
+			if ( z.compareTo(max) == 1 ) {
+				max = z;
+			}
+		}
+		System.out.println(max);
+	}	
+	
 	public static void main(String[] args) {
 		
 		//UC1 - checking maximum of 3 numbers
-		Integer x= 2 ; Integer y= 4 ; Integer z= 7;
+		Integer x = 2 ; Integer y = 4 ; Integer z = 7;
 		intmax(x,y,z);
+		
+		//UC2 - checking maximum of 3 floats
+		Float x1 = 8.2f ; Float y1 = 7.5f ; Float z1 = 6.5f;
+		floatmax(x1,y1,z1);
 	}
 }

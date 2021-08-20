06f47e5e02349beb303c3e5e3b130526f8282921
Devesh Bhangale1 <devesh.bhangale1@morningstar.com>
2021-08-20 21:24:42 +0530

    [Devesh]: Added maximum of 3 integer checking condition

--- src/com/maximumnumber/MaximumNumber.java
+++ src/com/maximumnumber/MaximumNumber.java
@@ -1,7 +1,29 @@
 package com.maximumnumber;
+import java.util.Scanner;
 
 public class MaximumNumber {
+	
+	public static void intmax(Integer x, Integer y, Integer z) {
+		Integer max = Integer.MIN_VALUE;
+		if ( x.compareTo(y) == 1 ) {
+			max = x;
+			if ( z.compareTo(max) == 1 ) {
+				max = z;
+			}
+		}
+		else {
+			max = y;
+			if ( z.compareTo(max) == 1 ) {
+				max = z;
+			}
+		}
+		System.out.println(max);
+	}
+	
 	public static void main(String[] args) {
 		
+		//UC1 - checking maximum of 3 numbers
+		Integer x= 2 ; Integer y= 4 ; Integer z= 7;
+		intmax(x,y,z);
 	}
 }
